d66d8ea0642e5076124d394cb51e86565fb36804
ninateng <niinateng@gmail.com>
2019-08-21 14:02:47 -0700

    FULLY WORKING NOW

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,25 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+var connection;
+
 //keys
 require("dotenv").config();
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.CONNECTION_ID,
-  database: "burgers_db"
-});
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection( process.env.JAWSDB_URL);
+
+}
+else {
+    connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.CONNECTION_ID,
+    database: "burgers_db"
+  });
+}
+
 
 // Make connection.
 connection.connect(function(err) {
